78bf9e0b8fe66a84bea8ba38b0c376a29d0fd934
Claire Duvallet <cduvallet@gmail.com>
2022-12-11 17:53:52 -0800

    Also fix the image prefix

--- _jupyter/convert_and_move.sh
+++ _jupyter/convert_and_move.sh
@@ -4,7 +4,15 @@ echo $nb
 jupyter nbconvert $nb --to markdown
 base_name=$(basename ${nb})
 
-echo "Moving ${nb%.ipynb}.md to ~/github/cduvallet.github.io/_posts/${base_name%.ipynb}.md"
-#mv ${nb%.ipynb}.md ~/github/cduvallet.github.io/_posts/${base_name%.ipynb}.md
-echo "Moving ${nb%.ipynb}_files to ~/github/cduvallet.github.io/images/${base_name%.ipynb}_files"
-#mv ${nb%.ipynb}_files ~/github/cduvallet.github.io/images/${base_name%.ipynb}_files
+markdown_file=${base_name%.ipynb}.md
+file_folder=${base_name%.ipynb}_files
+
+echo "Moving ${nb%.ipynb}.md to ~/github/cduvallet.github.io/_posts/${markdown_file}"
+mv ${nb%.ipynb}.md ~/github/cduvallet.github.io/_posts/${markdown_file}
+echo "Moving ${nb%.ipynb}_files to ~/github/cduvallet.github.io/images/${file_folder}"
+mv ${nb%.ipynb}_files ~/github/cduvallet.github.io/images/${file_folder}
+
+# Make the file names work - replace any instance of ![png]( with ![png](../images/
+sed -i .bak 's:\!\[png\](:\!\[png\](..images\/:' ~/github/cduvallet.github.io/_posts/${markdown_file}
+# Remove the backup sed made
+rm ~/github/cduvallet.github.io/_posts/${markdown_file}.bak
