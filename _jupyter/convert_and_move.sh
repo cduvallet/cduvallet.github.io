df8d4dc187cab2664ce46c181b39bad1f02788b5
Claire Duvallet <cduvallet@gmail.com>
2022-12-11 17:33:44 -0800

    More flexible file paths

--- _jupyter/convert_and_move.sh
+++ _jupyter/convert_and_move.sh
@@ -1,5 +1,10 @@
 nb=$1
 
+echo $nb
 jupyter nbconvert $nb --to markdown
-mv ${nb%.ipynb}.md ~/github/cduvallet.github.io/_posts/
-mv ${nb%.ipynb}_files ~/github/cduvallet.github.io/images/
+base_name=$(basename ${nb})
+
+echo "Moving ${nb%.ipynb}.md to ~/github/cduvallet.github.io/_posts/${base_name%.ipynb}.md"
+#mv ${nb%.ipynb}.md ~/github/cduvallet.github.io/_posts/${base_name%.ipynb}.md
+echo "Moving ${nb%.ipynb}_files to ~/github/cduvallet.github.io/images/${base_name%.ipynb}_files"
+#mv ${nb%.ipynb}_files ~/github/cduvallet.github.io/images/${base_name%.ipynb}_files
